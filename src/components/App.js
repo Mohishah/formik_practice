d8df1a75b22227fde93ec8d32cf1ae22725819f7
Mohammad <mohammad@Mohammads-MacBook-Air.local>
2024-08-18 17:30:45 +0330

    formik

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+
+const App = ()=>{
+    return (
+        <div>
+            {/* <Login/> */}
+            <Register/>
+        </div>
+    ) 
+}
+
+export default App;
+
+
+//npm i jalali-react-datepicker
